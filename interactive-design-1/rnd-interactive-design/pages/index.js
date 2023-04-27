bc0e2019ee30cfc788f671886cdef52a224861c1
Jasper Oh <ohyj0906@gmail.com>
2023-04-27 01:41:52 -0700

    Showing the result

--- interactive-design-1/rnd-interactive-design/pages/index.js
+++ interactive-design-1/rnd-interactive-design/pages/index.js
@@ -1,14 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import React, { useRef } from 'react'
+import Xarrow from 'react-xarrows'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const boxStyle = {border: "grey solid 2px", borderRadius: "10px", padding: "5px"};
+  const box1Ref = useRef(null);
   return (
-    <div>
-      hello mfasdfasdasdfasdfasdf
-    </div>
+    <>
+      <div style={{display: "flex", justifyContent: "space-evenly", width: "100%"}}>
+            <div ref={box1Ref} style={boxStyle}>hey</div>
+            <p id="elem2" style={boxStyle}>hey2</p>
+            <Xarrow
+                start={box1Ref} //can be react ref
+                end="elem2" //or an id
+            />
+        </div>
+    </>
   )
 }
