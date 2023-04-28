bb7c601f47772c062e0d8189850bf2207fecb03b
Jasper Oh <ohyj0906@gmail.com>
2023-04-27 22:32:08 -0700

    Successfully Done InteractiveDesign-1

--- interactive-design-1/rnd-interactive-design/pages/index.js
+++ interactive-design-1/rnd-interactive-design/pages/index.js
@@ -1,27 +1,83 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import React, { useRef } from 'react'
+import React, { useRef, useState} from 'react'
 React.useLayoutEffect = React.useEffect
-import Xarrow from 'react-xarrows'
+import { ArcherContainer, ArcherElement } from 'react-archer'
+import Xarrow from "react-xarrows";
 
 
 export default function Home() {
-  const boxStyle = {border: "grey solid 2px", borderRadius: "10px", padding: "5px", margin :"40px"};
+  const boxStyle = {border: "grey solid 2px", borderRadius: "10px", padding: "5px"};
   const box1Ref = useRef(null);
+
+  const [textAreaNo, setTextAreaNo] = useState(0);
+  const [uploadNo, setUploadNo] = useState(0);
+  const [arrowNo, setArrowNo] = useState(0);
+  let arrayThing = [ {
+    targetId : "element0",                    
+    sourceAnchor: "bottom",
+    targetAnchor : "top",
+    style: {strokeDasharray:'5,5'},
+  },
+  {
+    targetId : "element1",                    
+    sourceAnchor: "bottom",
+    targetAnchor : "top",
+    style: {strokeDasharray:'5,5'},
+  },
+  {
+    targetId : "element2",                    
+    sourceAnchor: "bottom",
+    targetAnchor : "top",
+    style: {strokeDasharray:'5,5'},
+  },];
   return (
     <>
-      <div style={{display: "flex", flexDirection:"column", justifyContent: "space-evenly", width: "50%" , textAlign:"center"}}>
-            <div ref={box1Ref} style={boxStyle}>hey</div>
-            <div style={{textAlign:"center"}}>
-            <button style={{width:"40px", marginLeft:"70px"}}>+</button> 
-            </div>
-            <p id="elem2" style={boxStyle}>hey2</p>
-            <Xarrow
-                start={box1Ref} //can be react ref
-                end="elem2" //or an id
-            />
-        </div>
+      <div style={{display: "flex", flexDirection:"column", justifyContent: "space-evenly", width: "50%" , textAlign:"center" ,alignItems:"center"}}>                  
+        <ArcherContainer strokeColor="red">
+            
+              <ArcherElement
+                id="root"
+                relations={arrayThing}
+                >
+                  <div style={boxStyle}>hey</div>
+                </ArcherElement>
+            
+            <div style={{height:"150px" , textAlign:"center" , display:"flex" ,justifyContent:"center" , alignItems:"center"}}>
+              <button style={{width:"40px", marginLeft:"70px", height:"40px"}} onClick={() => setTextAreaNo(textAreaNo+1)}>+</button> 
+            </div>       
+            <div style={{display: "flex", justifyContent: "space-evenly", textAlign:"center"}}>
+              {[...Array(textAreaNo)].map((a, i) => (
+                <ArcherElement
+                id={"element" + i}
+                >
+                  <div style={boxStyle}>element2</div>
+                </ArcherElement>              
+              ))}                            
+              </div>                
+        </ArcherContainer>
+      </div>
     </>
   )
 }
+
+
+{/* <div id="existing-thread" ref={box1Ref} style={boxStyle}>hey</div>
+
+<div style={{textAlign:"center"}}>
+  <button style={{width:"40px", marginLeft:"70px"}} onClick={() => setTextAreaNo(textAreaNo+1)}>+</button> 
+</div>
+<div style={{display: "flex", justifyContent: "space-evenly", textAlign:"center"}}>
+  {[...Array(textAreaNo)].map((a, i)=>(
+    <>
+      <p id={"thread"+i} style={boxStyle}>hey2</p>
+      <Xarrow
+        start="existing-thread" //can be react ref                    
+        end={"thread"+i} //or an id
+      />                                  
+    </>
+    ))}
+    
+    
+</div> */}
